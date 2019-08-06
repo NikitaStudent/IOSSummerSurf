2bf5e14c0d60d84263e188767817f213743b876d
Nikita <NKA_2000_00@mail.ru>
2019-08-06 15:40:40 +0300

    Init

---
+++ test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by Никита Кожевников on 06/08/2019.
+//  Copyright © 2019 Beet inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
